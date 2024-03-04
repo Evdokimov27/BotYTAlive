89434a62af5dcf352628259d4ae72f249aa318a7
Evdokimov27 <mr.evdokimov27@yandex.ru>
2024-03-05 00:45:32 +0300

    Global Update

--- YTVideoBot/Form1.Designer.cs
+++ YTVideoBot/Form1.Designer.cs
@@ -30,14 +30,15 @@
 		{
 			textBox1 = new TextBox();
 			button1 = new Button();
-			textBox2 = new TextBox();
 			label1 = new Label();
-			label2 = new Label();
+			button2 = new Button();
+			checkBox1 = new CheckBox();
+			checkBox2 = new CheckBox();
 			SuspendLayout();
 			// 
 			// textBox1
 			// 
-			textBox1.Location = new Point(12, 52);
+			textBox1.Location = new Point(12, 27);
 			textBox1.Multiline = true;
 			textBox1.Name = "textBox1";
 			textBox1.Size = new Size(222, 64);
@@ -45,7 +46,7 @@
 			// 
 			// button1
 			// 
-			button1.Location = new Point(85, 221);
+			button1.Location = new Point(12, 153);
 			button1.Name = "button1";
 			button1.Size = new Size(75, 23);
 			button1.TabIndex = 1;
@@ -53,44 +54,60 @@
 			button1.UseVisualStyleBackColor = true;
 			button1.Click += buttonSearch_Click;
 			// 
-			// textBox2
-			// 
-			textBox2.Location = new Point(70, 192);
-			textBox2.Name = "textBox2";
-			textBox2.Size = new Size(100, 23);
-			textBox2.TabIndex = 2;
-			// 
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Location = new Point(59, 34);
+			label1.Location = new Point(56, 9);
 			label1.Name = "label1";
 			label1.Size = new Size(143, 15);
 			label1.TabIndex = 3;
 			label1.Text = "Точное название ролика";
 			// 
-			// label2
+			// button2
 			// 
-			label2.AutoSize = true;
-			label2.Location = new Point(85, 174);
-			label2.Name = "label2";
-			label2.Size = new Size(75, 15);
-			label2.TabIndex = 4;
-			label2.Text = "Время в сек.";
+			button2.Location = new Point(12, 97);
+			button2.Name = "button2";
+			button2.Size = new Size(75, 44);
+			button2.TabIndex = 5;
+			button2.Text = "Выбрать папку";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// checkBox1
+			// 
+			checkBox1.AutoSize = true;
+			checkBox1.Location = new Point(93, 97);
+			checkBox1.Name = "checkBox1";
+			checkBox1.Size = new Size(53, 19);
+			checkBox1.TabIndex = 6;
+			checkBox1.Text = "Лайк";
+			checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			checkBox2.AutoSize = true;
+			checkBox2.Location = new Point(93, 122);
+			checkBox2.Name = "checkBox2";
+			checkBox2.Size = new Size(80, 19);
+			checkBox2.TabIndex = 7;
+			checkBox2.Text = "Подписка";
+			checkBox2.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(246, 261);
-			Controls.Add(label2);
+			ClientSize = new Size(246, 188);
+			Controls.Add(checkBox2);
+			Controls.Add(checkBox1);
+			Controls.Add(button2);
 			Controls.Add(label1);
-			Controls.Add(textBox2);
 			Controls.Add(button1);
 			Controls.Add(textBox1);
 			FormBorderStyle = FormBorderStyle.FixedToolWindow;
 			Name = "Form1";
 			Text = "Ботики YT живые";
+			FormClosing += Form1_FormClosing;
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -102,5 +119,8 @@
 		private TextBox textBox2;
 		private Label label1;
 		private Label label2;
+		private Button button2;
+		private CheckBox checkBox1;
+		private CheckBox checkBox2;
 	}
 }
--- YTVideoBot/Form1.cs
+++ YTVideoBot/Form1.cs
@@ -11,95 +11,80 @@ using OpenQA.Selenium.Support.Extensions;
 using SeleniumUndetectedChromeDriver;
 using OpenQA.Selenium.Chrome.ChromeDriverExtensions;
 using OpenQA.Selenium.DevTools.V120.FedCm;
+using System.Security.Policy;
+using System.Reflection.Metadata;
+using System.Collections.ObjectModel;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace YTVideoBot
 {
 	public partial class Form1 : Form
 	{
 		private UndetectedChromeDriver driver;
-		private WebDriverWait wait;
-		string[] mail = {
-
-	"sanjeevibeird@gmail.com",
-	"reappknowramistephanie@gmail.com",
-	"foleymegan47@gmail.com",
-	"lerimadonevans@gmail.com",
-	"sporhuacadubabu@gmail.com",
-	"troykeleher67@gmail.com",
-	"cremestasip1986@gmail.com",
-	"serscarrerenleslie@gmail.com",
-	"mieletkeysepam@gmail.com",
-	"exombronsouthmario@gmail.com",
-	"locarecgajaime@gmail.com",
-	"jimmypittman411@gmail.com",
-	"checkponcoger1979@gmail.com",
-	"enasobcyctiffany@gmail.com",
-	"lisahibbard110@gmail.com",
-	"allennina86@gmail.com",
-	"brocterssicna1979@gmail.com",
-	"mclainjack292@gmail.com",
-	"carranzasarah463@gmail.com",
-	"c3ifekasi@gmail.com",
-};
-
-		string[] password = {
-	"rQDuAWNaiyf",
-	"4bbYfILXMcoJ",
-	"IQGa1zPbwwiwR",
-	"wmhNeR7izhw3",
-	"KCxgqAiLpIJ64a",
-	"x64p6NZNq7Uj",
-	"PSeKFpFBrwRK",
-	"WIxJ9MSQTCDsQq",
-	"fKZzX3Ur50dJMS",
-	"44XN5DZ1GXgn",
-	"ghtya2LXFw9Q",
-	"UKZ97reyIxP",
-	"MMa7qCUneJSdh",
-	"lPL6mKwPh4hTd",
-	"Owwdn8QyMoE",
-	"XqHW39JQ4IuXk9",
-	"gf6zSwpJJNz",
-	"zfRr0xWVsrpA3g",
-	"cpXiTqrJVCG",
-	"QTQ8qhsnj47lt6",
-};
-
-		string[] proxy = { "65.109.112.245:9877", "51.195.51.101:3129" };
+		private string folder;
+		List<(string Email, string Password, string Proxy, string Login, string LoginPassword)> ReadAccountData(string filePath)
+		{
+			var accounts = new List<(string, string, string, string, string)>();
+			foreach (var line in File.ReadAllLines(filePath))
+			{
+				var parts = line.Split(';');
+				var account = (parts[0], parts[1], parts.Length > 2 ? parts[2] : "", parts.Length > 3 ? parts[3] : "", parts.Length > 4 ? parts[4] : "");
+				accounts.Add(account);
+			}
+			return accounts;
+		}
 		public Form1()
 		{
 			InitializeComponent();
+			folder = Properties.Settings.Default.Folder;
+			textBox1.Text = Properties.Settings.Default.VideoName;
 		}
-		private async void InitializeSeleniumWebDriver(string proxy)
+
+		private async void InitializeSeleniumWebDriver(string? proxy)
 		{
 			ChromeOptions chromeOptions = new ChromeOptions();
+			if (!string.IsNullOrEmpty(proxy))
+			{
+				chromeOptions.AddArgument($"--proxy-server={proxy}");
+			}
 			chromeOptions.PageLoadStrategy = PageLoadStrategy.None;
 			chromeOptions.AddArgument("--disable-blink-features=AutomationControlled");
-			chromeOptions.AddArgument($"--proxy-server=http://{proxy}");
+			chromeOptions.AddArgument("--mute-audio");
 			driver = UndetectedChromeDriver.Create(chromeOptions, null, driverExecutablePath: await new ChromeDriverInstaller().Auto());
-			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-			driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-			Thread.Sleep(5000);
 		}
 
-		private void OpenVideo(string videoName)
+		private async Task OpenVideoAsync(string videoName)
 		{
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
+
 			driver.Navigate().GoToUrl("https://www.youtube.com");
-			Thread.Sleep(5000);
-			((IJavaScriptExecutor)driver).ExecuteScript("window.stop();");
+			await Task.Delay(2000); // Используем асинхронную задержку вместо Thread.Sleep
 			var searchBox = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("search_query")));
 			searchBox.Click();
-			searchBox.SendKeys(videoName + OpenQA.Selenium.Keys.Enter);
+			searchBox.SendKeys(videoName);
+			await Task.Delay(1000); // Используем асинхронную задержку вместо Thread.Sleep
 
+			var search = wait.Until(ExpectedConditions.ElementExists(By.Id("search-icon-legacy")));
+			await Task.Delay(2000); // Используем асинхронную задержку вместо Thread.Sleep
+			search.Click();
 
 			var videoResult = wait.Until(ExpectedConditions.ElementExists(By.CssSelector("ytd-video-renderer,ytd-grid-video-renderer")));
 			if (videoResult != null)
 			{
 				videoResult.Click(); // Кликаем по первому видео в результате
-				Thread.Sleep(5000);
-				var like = wait.Until(ExpectedConditions.ElementExists(By.ClassName("YtLikeButtonViewModelHost")));
-				like.Click();
-				Thread.Sleep((int.Parse(textBox2.Text) * 1000) + 5000);
+				Thread.Sleep(3000);
+				if (checkBox1.Checked && GetVideoLike(driver) == "I like this")
+				{
+					var like = wait.Until(ExpectedConditions.ElementExists(By.ClassName("YtLikeButtonViewModelHost")));
+					like.Click();
+				}
+				if (checkBox2.Checked && GetVideoSub(driver))
+				{
+					var sub = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("subscribe-button-shape")));
+					sub.Click();
+				}
+				int durationInWholeSeconds = GetVideoDurationInSeconds(driver);
+				await Task.Delay((durationInWholeSeconds * 1000) + 5000); // Используем асинхронную задержку
 			}
 			else
 			{
@@ -107,36 +92,148 @@ namespace YTVideoBot
 			}
 			driver?.Quit();
 		}
+		public int GetVideoDurationInSeconds(IWebDriver driver)
+		{
+			IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+			string script = "return document.querySelector('video').duration;";
+			var durationInSeconds = jsExecutor.ExecuteScript(script);
+
+			return Convert.ToInt32(Math.Round(Convert.ToDouble(durationInSeconds)));
+		}
+
+		public string GetVideoLike(IWebDriver driver)
+		{
+			IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+			string script = @"
+			var parentElement = document.querySelector('.yt-spec-button-view-model');
+			var children = parentElement.children;
+			var titles = [];
+			for (var i = 0; i < children.length; i++)
+			{
+				if (children[i].title) 
+				{
+					titles.push(children[i].title);
+				}
+			}
+			return titles;
+			";
+			var titles = (ReadOnlyCollection<object>)jsExecutor.ExecuteScript(script);
+			string tile = "";
+			// Выводим полученные заголовки
+			foreach (var title in titles)
+			{
+				tile = title.ToString();
+			}
+			return tile;
+		}
+		public bool GetVideoSub(IWebDriver driver)
+		{
+			bool hasSubscribe = false; 
+			IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+			string script = @"
+			var elements = document.querySelectorAll('.yt-core-attributed-string.yt-core-attributed-string--white-space-no-wrap');
+			var texts = [];
+			for (var i = 0; i < elements.length && i < 4; i++)
+			{
+			    texts.push(elements[i].textContent.trim());
+			}
+			return texts;
+			";
+
+			// Выполнение JavaScript скрипта и получение результатов
+			var texts = (ReadOnlyCollection<object>)jsExecutor.ExecuteScript(script);
+
+			// Вывод текстов найденных элементов
+			foreach (var text in texts)
+			{
+				if (text.ToString() == "Subscribe")
+				{
+					hasSubscribe = true;
+				}
+			}
+			return hasSubscribe;
+		}
 		private async void Auth(string mail, string password)
 		{
-			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
-			driver.Navigate().GoToUrl("http://accounts.google.com/");
-			Thread.Sleep(5000);
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
+
+			bool isPageLoadedSuccessfully;
+			do
+			{
+				driver.Navigate().GoToUrl("http://accounts.google.com/");
+				Thread.Sleep(2000); // Задержка для загрузки страницы, лучше использовать явные ожидания
+
+				// Проверяем, есть ли на странице контейнер с ошибкой
+				var errorElements = driver.FindElements(By.Id("af-error-container"));
+				isPageLoadedSuccessfully = !errorElements.Any(); // Страница загружена успешно, если элемент с ошибкой не найден
+
+				if (!isPageLoadedSuccessfully)
+				{
+					Console.WriteLine("Обнаружена ошибка 400, пытаемся перезагрузить страницу...");
+					// Можно добавить задержку перед повторной попыткой, если это необходимо
+					Thread.Sleep(5000);
+				}
+			}
+			while (!isPageLoadedSuccessfully);
+
+			// Когда страница загружена успешно, выполняем дальнейшие действия
 			var Auth = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("identifierId")));
 			Auth.Click();
 			Auth.SendKeys(mail + OpenQA.Selenium.Keys.Enter);
-			Thread.Sleep(5000);
+
+			Thread.Sleep(2000); // Лучше использовать явные ожидания
+
 			var Pass = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("Passwd")));
 			Pass.Click();
 			Pass.SendKeys(password + OpenQA.Selenium.Keys.Enter);
-			Thread.Sleep(5000);
+			Thread.Sleep(5000); // Лучше использовать явные ожидания
+
+
 		}
-		private void buttonSearch_Click(object sender, EventArgs e)
+
+		private async void buttonSearch_Click(object sender, EventArgs e)
 		{
 			string videoName = textBox1.Text;
+			var accounts = ReadAccountData($"{folder}");
 			if (!string.IsNullOrWhiteSpace(videoName))
 			{
-				for (int i = 0; i < mail.Length; i++)
+				for (int i = 0; i < accounts.Count; i++)
 				{
-					InitializeSeleniumWebDriver(proxy[i]);
-					Auth(mail[i], password[i]);
-					OpenVideo(videoName);
+					InitializeSeleniumWebDriver(accounts[i].Proxy);
+					Auth(accounts[i].Email, accounts[i].Password);
+					await OpenVideoAsync(videoName);
 				}
 			}
 			else
 			{
 				MessageBox.Show("Введите название видео.");
 			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				if (folder != "") openFileDialog.InitialDirectory = folder;
+				else openFileDialog.InitialDirectory = "c:\\";
+				openFileDialog.Filter = "txt files (*.txt)|*.txt";
+				openFileDialog.FilterIndex = 2;
+				openFileDialog.RestoreDirectory = true;
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					string filePath = openFileDialog.FileName;
+					folder = filePath;
+				}
+				Properties.Settings.Default.Folder = folder;
+				Properties.Settings.Default.VideoName = textBox1.Text;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Properties.Settings.Default.Save();
+			driver?.Quit();
 		}
 	}
 }
